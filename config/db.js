232350a9a3d64c37807baff25591dbe49ea48af1
TristanG <tristan@gcans.com>
2024-01-30 13:52:31 -0500

    require ssl

--- config/db.js
+++ config/db.js
@@ -9,6 +9,10 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+    require: true,
+  },
 });
 
 pool.on("connect", () => {
