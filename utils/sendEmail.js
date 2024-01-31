57eede53683fb2a979c621f5ebb173cc73522ec7
TristanG <tristan@gcans.com>
2024-01-31 15:22:50 -0500

    check in

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SEND_EMAIL_USERNAME,
     pass: process.env.SEND_EMAIL_API_KEY,
