28e56539d73fa67ec9e57c910c8da8e26da363b7
TristanG <tristan@gcans.com>
2024-03-06 15:36:53 -0500

    check in: more playlist endpoints

--- models/Playlist.js
+++ models/Playlist.js
@@ -23,50 +23,10 @@ class Playlist {
     this.updatedAt = updatedAt;
   }
 
-  //   static async findById(id) {
-  //     try {
-  //       const playlist = await query(
-  //         `SELECT * FROM playlist JOIN game_playlist ON playlist.id = game_playlist.playlist_id WHERE id = $1`,
-  //         [id]
-
-  //         );
-
-  //         // get all games for a playlist
-
-  //       return playlist.rows[0];
-  //     } catch (error) {
-  //       console.error("Error fetching game by id:", error);
-  //       return null;
-  //     }
-  //   }
-
-  // static async findAllGamesByPlaylistId(playlistId) {
-  //     try {
-  //         const games = await query(`
-  //             SELECT games.id AS game_id, games.name AS game_name, games.description AS game_description,
-  //             games.is_public AS game_is_public, games.is_category AS game_is_category,
-  //             playlist.id AS playlist_id, playlist.owner_id AS playlist_owner_id,
-  //             playlist.name AS playlist_name, playlist.description AS playlist_description,
-  //             playlist.user_id AS playlist_user_id, playlist.is_public AS playlist_is_public,
-  //             playlist.is_category AS playlist_is_category, playlist.created_at AS playlist_created_at,
-  //             playlist.updated_at AS playlist_updated_at
-  //             FROM games
-  //             JOIN game_playlist ON games.id = game_playlist.game_id
-  //             JOIN playlist ON playlist.id = game_playlist.playlist_id
-  //             WHERE playlist.id = $1
-  //         `, [playlistId]);
-
-  //         return games.rows;
-  //     } catch (error) {
-  //         console.error("Error fetching games by playlist id:", error);
-  //         return null;
-  //     }
-  // }
-
   static async findById(id) {
     try {
       const playlist = await query(
-        `SELECT playlist.*, users.username AS ownername FROM playlist JOIN users ON playlist.owner_id = users.id WHERE id = $1`,
+        `SELECT playlist.*, users.username AS ownername FROM playlist JOIN users ON playlist.owner_id = users.id WHERE playlist.id = $1`,
         [id]
       );
       return playlist.rows[0];
@@ -76,7 +36,7 @@ class Playlist {
     }
   }
 
-  static async getAllGamesForSinglePlaylist(playlistId, next) {
+  static async getSinglePlaylist(playlistId, next) {
     try {
       const games = await query(
         `SELECT playlist.id, playlist.owner_id, users.username AS ownername, playlist.name, playlist.description, playlist.is_public, playlist.is_category, playlist.created_at, playlist.updated_at FROM playlist JOIN users ON playlist.owner_id = users.id WHERE playlist.id = $1`,
@@ -100,32 +60,50 @@ class Playlist {
     }
   }
 
-  // get all playlists for a user
-  //   static async findByUserId(userId) {
-  //     try {
-  //       const games = await query("SELECT * FROM playlist WHERE user_id = $1", [
-  //         userId,
-  //       ]);
-  //       return games.rows;
-  //     } catch (error) {
-  //       console.error("Error fetching games by user id:", error);
-  //       return null;
-  //     }
-  //   }
+  static async getAllPlaylistsInLibrary(userId, next) {
+    try {
+      const playlists = await query(
+        `SELECT playlist.id, playlist.owner_id, playlist.name, playlist.description, playlist.is_public, playlist.is_category, playlist.created_at, playlist.updated_at FROM playlist JOIN user_playlist ON playlist.id = user_playlist.playlist_id WHERE user_id = $1`,
+        [userId]
+      );
 
-  //   // get all games by tag name or id
-  //   static async findByTagId(tagId) {
-  //     try {
-  //       const games = await query(
-  //         "SELECT * FROM games JOIN game_tags ON games.id = game_tags.game_id WHERE game_tags.tag_id = $1",
-  //         [tagId]
-  //       );
-  //       return games.rows;
-  //     } catch (error) {
-  //       console.error("Error fetching games by tag id:", error);
-  //       return null;
-  //     }
-  //   }
+      const updatedPlaylists = await Promise.all(
+        playlists.rows.map(async (playlist) => {
+          const owner = await query(
+            `SELECT username FROM users WHERE id = $1`,
+            [playlist.owner_id]
+          );
+          playlist.username = owner.rows[0].username;
+          return playlist;
+        })
+      );
+
+      return updatedPlaylists;
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getAllGamesForSinglePlaylist(playlistId, next) {
+    try {
+      const games = await query(
+        `SELECT games.id, games.title, games.description, games.published, games.thumbnail, games.user_id, games.created_at, games.updated_at, COUNT(game_session.game_session_id) AS play_count FROM games JOIN game_playlist ON games.id = game_playlist.game_id LEFT JOIN game_session ON games.id = game_session.game_id WHERE game_playlist.playlist_id = $1 GROUP BY games.id`,
+        [playlistId]
+      );
+      const updatedGames = await Promise.all(
+        games.rows.map(async (game) => {
+          const user = await query(`SELECT username FROM users WHERE id = $1`, [
+            game.user_id,
+          ]);
+          game.username = user.rows[0].username;
+          return game;
+        })
+      );
+      return updatedGames;
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = Playlist;
--- routes/playlistRoutes.js
+++ routes/playlistRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const { query } = require("../config/db");
 const Playlist = require("../models/Playlist");
 const { authenticate } = require("../middleware/auth");
-const { placeholder } = require("../middleware/placeholder");
 const router = express.Router();
 
+// Create a new playlist
 router.post("/create", authenticate, async (req, res, next) => {
   const ownerId = req?.user?.id;
   const { name, description, isPublic } = req.body;
@@ -45,12 +45,71 @@ router.post(
       return res.status(400).json({ error: "Invalid request" });
     }
 
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) {
+      return res.status(404).json({ error: "Playlist not found" });
+    }
+
+    if (!playlist.is_public) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    if (playlist.owner_id !== req?.user?.id) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    // check if game is already in playlist
+    const gameExists = await query(
+      `SELECT * FROM game_playlist WHERE playlist_id = $1 AND game_id = $2`,
+      [playlistId, gameId]
+    );
+
+    if (gameExists.rows.length > 0) {
+      return res.status(400).json({ error: "Game already exists in playlist" });
+    }
+
     try {
       const newGamePlaylist = await query(
         `INSERT INTO game_playlist (playlist_id, game_id) VALUES ($1, $2) RETURNING *`,
         [playlistId, gameId]
       );
       res.status(201).json(newGamePlaylist.rows[0]);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// Remove a game from a playlist (game_playlist)
+router.delete(
+  "/:playlistId/remove/:gameId",
+  authenticate,
+  async (req, res, next) => {
+    const playlistId = req.params.playlistId;
+    const gameId = req.params.gameId;
+    const potentialOwnerId = req?.user?.id;
+
+    if (!playlistId || !gameId || !potentialOwnerId) {
+      return res.status(400).json({ error: "Invalid request" });
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) {
+      return res.status(404).json({ error: "Playlist not found" });
+    }
+
+    if (playlist.owner_id !== potentialOwnerId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    try {
+      await query(
+        `DELETE FROM game_playlist WHERE playlist_id = $1 AND game_id = $2`,
+        [playlistId, gameId]
+      );
+      res.status(200).json({ message: "Game removed from playlist" });
     } catch (error) {
       next(error);
     }
@@ -64,6 +123,12 @@ router.post("/:playlistId/add", authenticate, async (req, res, next) => {
 
   if (!playlistId || !userId) {
     return res.status(400).json({ error: "Invalid request" });
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist.is_public && playlist.owner_id !== userId) {
+    return res.status(401).json({ error: "Private Playlist: Unauthorized" });
   }
 
   // check if playlist exists for the user already
@@ -89,7 +154,62 @@ router.post("/:playlistId/add", authenticate, async (req, res, next) => {
   }
 });
 
-// Get all games for a playlist
+// Remove a playlist from a users library
+router.delete("/:playlistId/remove", authenticate, async (req, res, next) => {
+  const playlistId = req.params.playlistId;
+  const userId = req?.user?.id;
+
+  if (!playlistId || !userId) {
+    return res.status(400).json({ error: "Invalid request" });
+  }
+
+  try {
+    await query(
+      `DELETE FROM user_playlist WHERE playlist_id = $1 AND user_id = $2`,
+      [playlistId, userId]
+    );
+    res.status(200).json({ message: "Playlist removed from library" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Update a playlist
+router.put("/:playlistId", authenticate, async (req, res, next) => {
+  const playlistId = req.params.playlistId;
+  const ownerId = req?.user?.id;
+  const { name, description, isPublic } = req.body;
+
+  if (!playlistId || !ownerId) {
+    return res.status(400).json({ error: "Invalid request" });
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist || playlist.owner_id !== ownerId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    // if isPublic is false but there are records in user_playlist, delete them
+    if (!isPublic) {
+      await query(`DELETE FROM user_playlist WHERE playlist_id = $1`, [
+        playlistId,
+      ]);
+    }
+
+    const updatedPlaylist = await query(
+      `UPDATE playlist SET name = $1, description = $2, is_public = $3 WHERE id = $4 AND owner_id = $5 RETURNING *`,
+      [name, description, isPublic, playlistId, ownerId]
+    );
+
+    res.status(200).json(updatedPlaylist.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Get a single playlist by its id
 router.get("/:playlistId", async (req, res, next) => {
   const playlistId = req.params.playlistId;
 
@@ -98,7 +218,7 @@ router.get("/:playlistId", async (req, res, next) => {
   }
 
   try {
-    const games = await Playlist.getAllGamesForSinglePlaylist(playlistId);
+    const games = await Playlist.getSinglePlaylist(playlistId);
 
     res.status(200).json(games);
   } catch (error) {
@@ -122,6 +242,64 @@ router.get("/user/:userId", async (req, res, next) => {
     console.log("playlists::", playlists);
 
     res.status(200).json(playlists);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Get all playlists in a users library (user_playlist)
+router.get("/library/:userId", authenticate, async (req, res, next) => {
+  const userId = req?.user?.id;
+
+  if (!userId) {
+    return res.status(400).json({ error: "Invalid request" });
+  }
+
+  try {
+    const playlists = await Playlist.getAllPlaylistsInLibrary(userId, next);
+
+    res.status(200).json(playlists);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Get all games for a playlist
+router.get("/:playlistId/games", async (req, res, next) => {
+  const playlistId = req.params.playlistId;
+
+  if (!playlistId) {
+    return res.status(400).json({ error: "Invalid request" });
+  }
+
+  try {
+    const games = await Playlist.getAllGamesForSinglePlaylist(playlistId, next);
+    const playlist = await Playlist.getSinglePlaylist(playlistId, next);
+
+    res.status(200).json({ ...playlist, games });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete a single playlist
+router.delete("/:playlistId", authenticate, async (req, res, next) => {
+  const playlistId = req.params.playlistId;
+  const ownerId = req?.user?.id;
+
+  if (!playlistId || !ownerId) {
+    return res.status(400).json({ error: "Invalid request" });
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist || playlist.owner_id !== ownerId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    await query(`DELETE FROM playlist WHERE id = $1`, [playlistId]);
+    res.status(200).json({ message: "Playlist deleted" });
   } catch (error) {
     next(error);
   }
